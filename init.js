98b000450bcce08f7cf0923d59addb9950513895
Estefanía <silestagui@gmail.com>
2020-11-24 16:50:36 +0100

    primer commit

---
+++ init.js
@@ -0,0 +1,11 @@
+ var firebaseConfig = {
+	    apiKey: "AIzaSyBjchOuBWAgKXlSJ0t6KFitvNcdoaLyCeY",
+	    authDomain: "usuarios-866e8.firebaseapp.com",
+	    databaseURL: "https://usuarios-866e8.firebaseio.com",
+	    projectId: "usuarios-866e8",
+	    storageBucket: "usuarios-866e8.appspot.com",
+	    messagingSenderId: "69706123147",
+	    appId: "1:69706123147:web:e8652ef9b6eaab4ed8d18e"
+	  };
+	  // Initialize Firebase
+	  firebase.initializeApp(firebaseConfig);
